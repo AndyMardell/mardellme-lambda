398a5a62c70cdc41d12bd2017a4c1ff8c8e12975
Andy Mardell <andy@sidigital.co>
2020-04-05 20:02:23 +0100

    Debug DynamoDB

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ exports.handler = async (event, _, callback) => {
 
   try {
     const data = await handleRequest(requestBody)
+    console.log('No Error', { data })
     callback(null, generateResponse({ data }, 200))
   } catch (err) {
     if (err.errors) {
       const response = generateResponse({ error: 'Input error' }, 400)
+      console.error('Input Error')
       return callback(null, response)
     }
 
     const response = generateResponse({ error: 'Internal server error' }, 500)
+    console.error('Server Error')
     callback(null, response)
   }
 }
